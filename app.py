d8fd4fce2074fb895e6d5a3ea1d7af82120f277e
KhaNiwat <kchaos1150@gmail.com>
2026-03-27 00:37:17 +0700

    แก้ไขข้อความ

--- app.py
+++ app.py
@@ -22,18 +22,18 @@ col1, col2 = st.columns([2, 1])
 
 
 # ส่วนที่ 2: ไฮไลท์ของแต่ละโมเดล (Quick Links / Summary)
-st.subheader("🚀 โมเดลที่พัฒนา")
+st.subheader("โมเดลที่พัฒนา")
 
 m_col1, m_col2 = st.columns(2)
 
 with m_col1:
-    st.markdown("### 🤖 Machine Learning")
+    st.markdown("### Machine Learning")
     st.write("ใช้ **Voting Classifier** (Logistic Regression, RF, SVM) เพื่อความแม่นยำและการตัดสินใจแบบกลุ่ม")
     if st.button("ไปที่หน้า Machine Learning"):
         st.switch_page("pages/1_Machine Learning.py")
 
 with m_col2:
-    st.markdown("### 🧠 Neural Network")
+    st.markdown("### Neural Network")
     st.write("ใช้ **Multi-Layer Perceptron (MLP)** โครงข่ายประสาทเทียมเพื่อหาความสัมพันธ์เชิงลึกของข้อมูล")
     if st.button("ไปที่หน้า Neural Network"):
         st.switch_page("pages/2_Neural_Network.py")
